6bf6617e4485c546f0583d35e796e062ad6ddb6d
JoseErisvaldo <100242848+JoseErisvaldo@users.noreply.github.com>
2023-07-01 23:38:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let valor = document.getElementById('valor')
+let unidade = document.getElementById('unidade')
+let produto = document.getElementById('produto')
+let receber = document.getElementById('card-pai')
+
+let arrayCompras = []
+
+function atualizar() { 
+    arrayCompras.push(Number(valor.value * unidade.value))
+
+    let newProduto = document.createElement('div')
+
+    let div = document.createElement('div')
+    div.classList.add('cardFilho')
+    div.textContent += `Descrição: ${produto.value}`
+
+    div.innerHTML += `Valor: ${baseCalculo(Number(valor.value), Number(unidade.value)).toFixed(2)}`
+
+    div.appendChild(newProduto)
+
+    receber.appendChild(div)
+
+
+
+    somar()
+}
+
+function baseCalculo(a, b) {
+    return a * b
+}
+
+
+function somar() {
+    let receberSoma = document.getElementById('soma')
+    let sum = arrayCompras.reduce((valorAcumulado, numeros) => valorAcumulado + numeros, 0)
+
+    receberSoma.innerHTML = sum
+
+    console.log(sum)
+    
+}
+
+somar()
